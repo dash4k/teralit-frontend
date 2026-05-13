f9a352323b4cc8b08407507044456f166e818060
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-13 18:28:09 +0700

    improved footer

--- src/components/PageFooter.jsx
+++ src/components/PageFooter.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 
 function PageFooter() {
   return (
-    <footer className='flex flex-row justify-between items-center w-full px-12 pb-3 pt-6 bg-surface-container-highest'>
-      <div className='flex flex-col items-center md:items-start gap-xs'>
-        <h2 className='text-headline-md font-display font-bold text-on-surface'>Teralit</h2>
-        <p className='font-body-sm text-body-sm text-on-surface-variant'>&#xA9; 2026 CC26-PSU247 Coding Camp</p>
-      </div>
-      <div className='font-body-sm text-body-sm text-on-surface-variant'>
-        <ul className='flex flex-row gap-5'>
-          <li>Privacy Policy</li>
-          <li>Terms of Service</li>
-          <li>Medical Disclaimer</li>
-          <li>Contact Support</li>
-        </ul>
+    <footer className='w-full pb-4 pt-6 bg-surface-container-highest dark:bg-inverse-surface'>
+      <div className='flex flex-row justify-between items-center max-w-[1200px] mx-auto px-container-margin'>
+        <div className='flex flex-col items-center md:items-start gap-xs'>
+          <h2 className='text-headline-md font-display font-bold text-on-surface dark:text-inverse-on-surface'>Teralit</h2>
+        </div>
+        <div className='font-body-sm text-body-sm text-on-surface-variant dark:text-inverse-on-surface'>
+          <ul className='flex flex-row gap-5'>
+            <li>Privacy Policy</li>
+            <li>Terms of Service</li>
+            <li>Medical Disclaimer</li>
+            <li>Contact Support</li>
+          </ul>
+        </div>
+        <div className='flex flex-col items-center md:items-start gap-xs'>
+          <p className='font-body-sm text-body-sm text-on-surface-variant dark:text-inverse-on-surface'>&#xA9; 2026 CC26-PSU247 Coding Camp</p>
+        </div>
       </div>
     </footer>
   );
