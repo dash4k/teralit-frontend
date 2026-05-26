0ec893a6146715eebf680f874f5c4f289f934a41
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-26 18:40:30 +0700

    chore: added user api integration

---
+++ src/utils/api/user.js
@@ -0,0 +1,58 @@
+import fetchWithToken from './fetchWithToken.js';
+
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
+export const viewProfile = async () => {
+  const response = await fetchWithToken(`${BASE_URL}/profile`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const responseJson = await response.json();
+
+  if (responseJson.status !== 'success') {
+    alert(responseJson.message);
+    return { error: true, data: null };
+  }
+
+  return { error: false, data: responseJson.data };
+};
+
+export const editProfile = async ({ name }) => {
+  const response = await fetchWithToken(`${BASE_URL}/profile`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name }),
+  });
+
+  const responseJson = await response.json();
+
+  if (responseJson.status !== 'success') {
+    alert(responseJson.message);
+    return { error: true };
+  }
+
+  return { error: false };
+};
+
+export const removeAccount = async () => {
+  const response = await fetchWithToken(`${BASE_URL}/users`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const responseJson = await response.json();
+
+  if (responseJson.status !== 'success') {
+    alert(responseJson.message);
+    return { error: true };
+  }
+
+  return { error: false };
+};
