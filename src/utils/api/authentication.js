ea5451b8722db96a9c8a360c66dc01269453144f
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-26 11:48:25 +0700

    chore: added authentication api integration

---
+++ src/utils/api/authentication.js
@@ -0,0 +1,119 @@
+import { getRefreshToken } from './localStorage.js';
+import fetchWithToken from './fetchWithToken.js';
+
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
+export const register = async ({ email, password, name }) => {
+  const response = await fetch(`${BASE_URL}/register`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password, name }),
+  });
+
+  const responseJson = await response.json();
+
+  if (responseJson.status !== 'success') {
+    alert(responseJson.message);
+    return { error: true };
+  }
+
+  return { error: false };
+};
+
+export const verifyEmail = async (verifyToken) => {
+  const response = await fetch(`${BASE_URL}/verify-email?token=${verifyToken}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const responseJson = await response.json();
+
+  if (responseJson.status !== 'success') {
+    alert(responseJson.message);
+    return { error: true };
+  }
+
+  return { error: false };
+};
+
+export const resendVerification = async ({ email }) => {
+  const response = await fetch(`${BASE_URL}/resend-verification`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email }),
+  });
+
+  const responseJson = await response.json();
+
+  if (responseJson.status !== 'success') {
+    alert(responseJson.message);
+    return { error: true };
+  }
+
+  return { error: false };
+};
+
+export const login = async ({ email, password }) => {
+  const response = await fetch(`${BASE_URL}/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  const responseJson = await response.json();
+
+  if (responseJson.status !== 'success') {
+    alert(responseJson.message);
+    return { error: true, data: null };
+  }
+
+  return { error: false, data: responseJson.data };
+};
+
+export const refreshToken = async () => {
+  const token = getRefreshToken();
+  const response = await fetchWithToken(`${BASE_URL}/authentications`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ token }),
+  });
+
+  const responseJson = await response.json();
+
+  if (responseJson.status !== 'success') {
+    alert(responseJson.message);
+    return { error: true, data: null };
+  }
+
+  return { error: false, data: responseJson.data };
+};
+
+export const logout = async () => {
+  const token = getRefreshToken();
+  const response = await fetchWithToken(`${BASE_URL}/authentications`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ token }),
+  });
+
+  const responseJson = await response.json();
+
+  if (responseJson.status !== 'success') {
+    alert(responseJson.message);
+    return { error: true };
+  }
+
+  return { error: false };
+};
