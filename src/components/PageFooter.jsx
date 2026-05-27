67fa487d4ad7b1f08ee39ef98d3fc53f399899bb
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-28 00:22:24 +0700

    chore: redesign Footer

--- src/components/PageFooter.jsx
+++ src/components/PageFooter.jsx
@@ -1,14 +1,30 @@
 function PageFooter() {
   return (
     <footer className='w-full pb-4 pt-6 bg-surface-container-highest dark:bg-inverse-surface'>
-      <div className='flex flex-col gap-lg md:flex-row justify-between md:items-center max-w-250 mx-auto px-container-margin'>
+      <div className='flex flex-col gap-lg md:flex-row justify-between max-w-250 mx-auto px-container-margin'>
         <h2 className='text-headline-md font-display font-bold text-on-surface dark:text-inverse-on-surface'>Teralit</h2>
         <div className='font-body-sm text-body-sm text-on-surface-variant dark:text-inverse-on-surface'>
-          <ul className='flex flex-row flex-wrap gap-5'>
-            <li>Privacy Policy</li>
-            <li>Terms of Service</li>
-            <li>Medical Disclaimer</li>
-            <li>Contact Support</li>
+          <ul className='flex md:flex-col flex-wrap gap-5'>
+            <li>Lorem Ipsum</li>
+            <li>Lorem Ipsum</li>
+            <li>Lorem Ipsum</li>
+            <li>Lorem Ipsum</li>
+          </ul>
+        </div>
+        <div className='font-body-sm text-body-sm text-on-surface-variant dark:text-inverse-on-surface'>
+          <ul className='flex md:flex-col flex-wrap gap-5'>
+            <li>Lorem Ipsum</li>
+            <li>Lorem Ipsum</li>
+            <li>Lorem Ipsum</li>
+            <li>Lorem Ipsum</li>
+          </ul>
+        </div>
+        <div className='font-body-sm text-body-sm text-on-surface-variant dark:text-inverse-on-surface'>
+          <ul className='flex md:flex-col flex-wrap gap-5'>
+            <li>Lorem Ipsum</li>
+            <li>Lorem Ipsum</li>
+            <li>Lorem Ipsum</li>
+            <li>Lorem Ipsum</li>
           </ul>
         </div>
         <p className='font-body-sm text-body-sm text-on-surface-variant dark:text-inverse-on-surface'>&#xA9; 2026 CC26-PSU247 Coding Camp</p>
