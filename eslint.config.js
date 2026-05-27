9f57da10700d05b0009ffe44758d203923c8d8b0
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-28 00:13:34 +0700

    chore: updated eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,26 @@
 import js from '@eslint/js';
 import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import { defineConfig, globalIgnores } from 'eslint/config';
+import { defineConfig } from 'eslint/config';
 import daStyle from 'eslint-config-dicodingacademy';
 
 export default defineConfig([
   daStyle,
-  globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
     languageOptions: {
-      globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+      globals: { ...globals.browser, ...globals.node }
     },
+    rules: {
+      'eol-last': ['error', 'always'],
+      'no-unused-vars': ['error', {
+        vars: 'all',
+        args: 'all',
+        argsIgnorePattern: '^_',
+        caughtErrors: 'all',
+        caughtErrorsIgnorePattern: '^_',
+      }],
+    }
   },
 ]);
