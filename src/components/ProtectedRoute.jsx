4b4de0afd5c92bdd67d6bbe1404a5134effa0c91
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-28 00:22:54 +0700

    chore: added route protecter component

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,8 @@
+import { Navigate } from "react-router-dom"
+
+const ProtectedRoute = ({ authedUser, children }) => {
+    if (authedUser === null) return <Navigate to='/login' replace />;
+    return children;
+};
+
+export default ProtectedRoute;
