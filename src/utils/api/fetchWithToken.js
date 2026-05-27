e6b84f6cabc1b7f9887da8a588d82003bc1b4d4b
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-28 00:28:16 +0700

    chore: added refresh token when fetch failed

--- src/utils/api/fetchWithToken.js
+++ src/utils/api/fetchWithToken.js
@@ -1,13 +1,30 @@
-import { getAccessToken } from './localStorage.js';
+import { getAccessToken, putAccessToken, putRefreshToken } from './localStorage.js';
 
 const fetchWithToken = async (url, options = {}) => {
-  return fetch(url, {
+  const response = fetch(url, {
     ...options,
     headers: {
       ...options.headers,
       Authorization: `Bearer ${getAccessToken()}`,
     },
   });
+
+  if (response.status === 401) {
+    const { accessToken, refreshToken } = await refreshToken();
+
+    putRefreshToken(refreshToken);
+    putAccessToken(accessToken);
+
+    return fetch(url, {
+      ...options,
+      headers: {
+        ...options.headers,
+        Authorization: `Bearer ${getAccessToken}`,
+      },
+    });
+  }
+
+  return response;
 };
 
 export default fetchWithToken;
