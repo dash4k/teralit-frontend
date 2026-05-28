f38b7f641944efeb75de888db8eb824312b19137
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-29 01:52:07 +0700

    chore: refactor IconButton

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -1,5 +1,9 @@
 const IconButton = ({ children, onClick }) => {
-    return <button className='text-on-surface-variant dark:text-inverse-primary pt-2 cursor-pointer' onClick={onClick}>{children}</button>
+    return <button className='
+            text-primary dark:text-inverse-primary pt-2 cursor-pointer
+        ' 
+        onClick={onClick}>{children}
+    </button>
 }
 
 export default IconButton;
