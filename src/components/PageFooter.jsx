460181e27373ac8f6323f0b00f0654a2df099e4d
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-29 01:53:17 +0700

    chore: cleaner code

--- src/components/PageFooter.jsx
+++ src/components/PageFooter.jsx
@@ -1,4 +1,10 @@
-function PageFooter() {
+import { matchPath, useLocation } from "react-router-dom";
+
+const PageFooter = () => {
+  const location = useLocation();
+  const isActive = (path) => !!matchPath({ path, end: false }, location.pathname);
+  if (isActive('/new') || isActive('/results/*') || isActive('/profile')) return null;
+
   return (
     <footer className='w-full pb-4 pt-6 bg-surface-container-highest dark:bg-inverse-surface'>
       <div className='flex flex-col gap-lg md:flex-row justify-between max-w-250 mx-auto px-container-margin'>
@@ -27,7 +33,9 @@ function PageFooter() {
             <li>Lorem Ipsum</li>
           </ul>
         </div>
-        <p className='font-body-sm text-body-sm text-on-surface-variant dark:text-inverse-on-surface'>&#xA9; 2026 CC26-PSU247 Coding Camp</p>
+        <p className='font-body-sm text-body-sm text-on-surface-variant dark:text-inverse-on-surface'>
+          &#xA9; 2026 CC26-PSU247 Coding Camp
+        </p>
       </div>
     </footer>
   );
