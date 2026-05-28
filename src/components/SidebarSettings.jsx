407fab5aa6ab8c91be746fd4d37609468ac1b910
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-29 01:55:38 +0700

    chore: added sidebar settings component

---
+++ src/components/SidebarSettings.jsx
@@ -0,0 +1,61 @@
+import { Link } from "react-router-dom"
+import { FaAddressCard, FaPowerOff } from "react-icons/fa6";
+import Button from "./Button"
+import toast from "react-hot-toast";
+
+const SidebarSettings = ({ 
+    settingsVisibility, 
+    logout, 
+    setSettingsVisibility 
+}) => {
+    const onlogout = () => {
+        setSettingsVisibility(false)
+        toast.promise(logout(), {
+            loading: 'Logging off your account...',
+            success: 'Logout success!',
+            error: (err) => err?.message ?? 'Error while trying to logout',
+        });
+        
+    }
+    return (
+        <div className={
+            `${settingsVisibility ? 'opacity-100' : 'opacity-0'} 
+            fixed bottom-15 left-5 z-50 rounded-md bg-background 
+            dark:bg-on-background w-50 h-auto flex flex-col justify-center 
+            items-center transition-opacity duration-75 border-outline border`
+        }>
+            <Link 
+                to='/profile' 
+                onClick={() => setSettingsVisibility(false)} 
+                className="w-full"
+            >
+                <Button 
+                    startIcon={<FaAddressCard />} 
+                    variant="ghost" 
+                    fullWidth={true} 
+                    className="
+                        justify-start pl-1 text-on-surface 
+                        dark:text-inverse-on-surface rounded-t-md 
+                        rounded-b-none
+                    "
+                >
+                    View Profile
+                </Button>
+            </Link>
+            <Button 
+                startIcon={<FaPowerOff />} 
+                onClick={onlogout} 
+                variant="ghost" 
+                fullWidth={true} 
+                className="
+                    justify-start pl-1 dark:text-inverse-on-surface 
+                    rounded-b-md rounded-t-none
+                "
+            >
+                Logout
+            </Button>
+        </div>
+    );
+};
+
+export default SidebarSettings;
