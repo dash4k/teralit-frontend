88501cd89f1a18eb03eb95476b7376e1e9d9b52a
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-31 09:40:55 +0700

    chore: added classification utility functions

---
+++ src/utils/utilities/classification.js
@@ -0,0 +1,13 @@
+export const CLASSIMAGE = {
+  ChickenPox: '/classifications/chickenPox.jpg',
+  Eczema: '/classifications/eczema.jpg',
+  Hives: '/classifications/hives.jpg',
+  Melanoma: '/classifications/melanoma.jpg',
+};
+
+export const CLASSLABEL = {
+  ChickenPox: 'Chicken Pox',
+  Eczema: 'Eczema',
+  Hives: 'Hives',
+  Melanoma: 'Melanoma',
+};
