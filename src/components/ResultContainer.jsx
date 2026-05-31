57fdacac151a989f22cd0733c1279655be5a036d
Danishwara Pracheta <danishwara81@gmail.com>
2026-05-31 09:42:25 +0700

    chore: added label formatting for diagnosis

--- src/components/ResultContainer.jsx
+++ src/components/ResultContainer.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import BodyText from './BodyText.jsx';
 import SessionStatus from './SessionStatus.jsx';
+import { CLASSLABEL } from '../utils/utilities/classification.js';
 
 const ResultContainer = ({ session }) => {
   const navigate = useNavigate();
@@ -14,7 +15,7 @@ const ResultContainer = ({ session }) => {
       className="relative w-full h-auto flex flex-row justify-between items-center bg-surface-container dark:bg-inverse-surface hover:-translate-y-1/12 hover:drop-shadow-lg transition-all duration-200 shadow-inverse-surface dark:shadow-surface hover:cursor-pointer"
     >
       <div className="flex-1 flex flex-col justify-center items-start border border-primary border-dashed p-2 rounded-DEFAULT">
-        <BodyText>{session.diagnosis}</BodyText>
+        <BodyText>{CLASSLABEL[session.diagnosis]}</BodyText>
         <p className="text-label-bold font-label-bold text-on-surface dark:text-inverse-on-surface">ID: {session.id}</p>
         <div className="flex flex-row justify-center items-start gap-container-margin">
           <BodyText>Created at: {new Date(session.createdAt).toLocaleString()}</BodyText>
