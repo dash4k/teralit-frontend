ed6f0d061732714241815b6ad37496e022493706
Danishwara Pracheta <danishwara81@gmail.com>
2026-06-01 09:24:10 +0700

    fix: removed react-config rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,16 +2,12 @@ import js from '@eslint/js';
 import globals from 'globals';
 import { defineConfig } from 'eslint/config';
 import daStyle from 'eslint-config-dicodingacademy';
-import reactPlugin from 'eslint-plugin-react';
 
 export default defineConfig([
   js.configs.recommended,
   daStyle,
   {
     files: ['**/*.{js,mjs,cjs,jsx}'],
-    plugins: {
-      react: reactPlugin
-    },
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
       parserOptions: {
@@ -30,34 +26,6 @@ export default defineConfig([
         caughtErrors: 'all',
         caughtErrorsIgnorePattern: '^_',
       }],
-
-      'react/jsx-uses-react': 'error',
-      'react/jsx-uses-vars': 'error',
-
-      'react/self-closing-comp': ['error', {
-        component: true,
-        html: true,
-      }],
-
-      'react/jsx-wrap-multilines': ['error', {
-        declaration: 'parens-new-line',
-        assignment: 'parens-new-line',
-        return: 'parens-new-line',
-        arrow: 'parens-new-line',
-        condition: 'parens-new-line',
-        logical: 'parens-new-line',
-      }],
-
-      'react/jsx-curly-brace-presence': ['error', {
-        props: 'never',
-        children: 'never',
-      }],
-
-      'react/jsx-pascal-case': 'error',
-      'react/jsx-closing-bracket-location': ['error', 'line-aligned'],
-      'react/jsx-first-prop-new-line': ['error', 'multiline'],
-      'react/jsx-max-props-per-line': ['error', { maximum: 1, when: 'always' }],
-      'react/jsx-boolean-value': ['error', 'never'],
     },
   },
 ]);
