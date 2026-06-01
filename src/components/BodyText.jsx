f8e0c68661d47565ecb33c446ccd18e93522c73a
Danishwara Pracheta <danishwara81@gmail.com>
2026-06-01 12:26:58 +0700

    chore: body text accepted custom class

--- src/components/BodyText.jsx
+++ src/components/BodyText.jsx
@@ -1,5 +1,5 @@
-const BodyText = ({ children }) => {
-  return <p className="font-body-md text-body-md text-on-surface-variant dark:text-inverse-on-surface">{children}</p>;
+const BodyText = ({ children, className }) => {
+  return <p className={`font-body-md text-body-md text-on-surface-variant dark:text-inverse-on-surface ${className}`}>{children}</p>;
 };
 
 export default BodyText;
