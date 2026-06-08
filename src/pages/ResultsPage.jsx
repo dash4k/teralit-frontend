5e3bc3e446046b1891fc9cf12ea1ee9f9cfa95f5
Danishwara Pracheta <danishwara81@gmail.com>
2026-06-08 22:41:30 +0700

    feat: implemented pagination on results page

--- src/pages/ResultsPage.jsx
+++ src/pages/ResultsPage.jsx
@@ -1,31 +1,45 @@
+import toast from 'react-hot-toast';
 import { useEffect, useState } from 'react';
 import { listSessions } from '../utils/api/session';
-import ResultContainer from '../components/ResultContainer.jsx';
+import { FaFileMedical, FaChevronRight, FaChevronLeft } from 'react-icons/fa6';
 import LoadingIcon from '../components/LoadingIcon.jsx';
-import { FaFileMedical } from 'react-icons/fa6';
+import ResultContainer from '../components/ResultContainer.jsx';
 import Heading from '../components/Heading.jsx';
-import toast from 'react-hot-toast';
+import Button from '../components/Button.jsx';
 
 const ResultsPage = () => {
   const [sessions, setSessions] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [disablePrev, setDisablePrev] = useState(false);
+  const [disableNext, setDisableNext] = useState(false);
+  const limit = 5;
+
+  const nextPage = () => setPage((prev) => prev + 1);
+  const prevPage = () => setPage((prev) => prev - 1);
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       try {
-        const { sessions } = await listSessions();
-        console.log(sessions);
+        const { data } = await listSessions({ page, limit });
+        const { sessions, totalPages } = data;
         setSessions(sessions);
-        toast.success('Data loaded successfully');
+        setTotalPages(totalPages);
       } catch (error) {
         toast.error(error?.message ?? 'Error while trying to load data');
       } finally {
+        setDisablePrev(false);
+        setDisableNext(false);
         setLoading(false);
+        if (page === 1) setDisablePrev(true);
+        if (page >= totalPages) setDisableNext(true);
       }
     };
 
     fetchData();
-  }, []);
+  }, [page]);
 
   if (loading) return (
     <section className="w-full min-h-dvh lg:h-auto px-container-margin mx-auto flex flex-col justify-center items-center text-on-surface dark:text-inverse-on-surface">
@@ -42,7 +56,6 @@ const ResultsPage = () => {
           </div>
           <p className="text-label-md font-label-md text-primary dark:text-inverse-primary flex gap-sm">Your past analysis sessions <FaFileMedical /></p>
         </div>
-
         <div className="w-full flex flex-col gap-sm">
           {sessions.map((session) => (
             <ResultContainer
@@ -51,7 +64,23 @@ const ResultsPage = () => {
             />
           ))}
         </div>
-
+        <div className="flex flex-row justify-end items-center w-full h-auto gap-2">
+          <Button
+            onClick={prevPage}
+            disabled={disablePrev}
+            variant='outlined'
+            startIcon={<FaChevronLeft />}
+          >
+            Prev
+          </Button>
+          <Button
+            onClick={nextPage}
+            disabled={disableNext}
+            endIcon={<FaChevronRight />}
+          >
+            Next
+          </Button>
+        </div>
       </div>
     </section>
   );
